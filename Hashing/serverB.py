49a1e94b7aa944a9d2878f20ba8a0f7f8ebf8d8f
inorawan <141328637+inorawan@users.noreply.github.com>
2024-04-03 10:32:59 +0530

    Update serverB.py

--- Hashing/serverB.py
+++ Hashing/serverB.py
@@ -1,1 +1,18 @@
+import socket
 
+PORT = 8081
+SERVER_ADDRESS = "127.0.0.1"
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(("0.0.0.0", PORT))
+
+server_socket.listen()
+print(f"Server is listening on port {PORT}")
+client_socket, client_address = server_socket.accept()
+
+
+file = client_socket.recv(4096).decode()
+response = open('server_files/' + file).read()
+client_socket.sendall(response.encode())
+
+client_socket.close()
